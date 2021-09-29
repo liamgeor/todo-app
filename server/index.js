7de23bfaa4acc708a14de3c87c3f9a475a74a2c2
Liam Geogre <14liamgeorge@gmail.com>
2021-09-29 14:18:32 -0400

    front and backend setup

--- server/index.js
+++ server/index.js
@@ -1,3 +1,19 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json({limit: "30mb", extended: true}))
+app.use(express.urlencoded({limit: "30mb", extended: true}))
+
+app.use(cors());
+
+const PORT = process.env.port || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error))
